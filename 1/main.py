1edc0cb44c7ce605adb71cd2fb1131cff1680dfb
Ougi <demon_284@bk.ru>
2021-11-12 23:16:54 +0300

    достойная реализация каждого метода но можно всё улучшить

--- 1/main.py
+++ 1/main.py
@@ -4,6 +4,7 @@ from math import log, e, fabs
 
 # 2-x=ln(x)
 class bisekta:
+    # метод бисекции
     def __init__(self, a, b):
         self.a = a
         self.b = b
@@ -68,68 +69,112 @@ class bisekta:
             print(tabulate(self.table, tablefmt='pipe', stralign='center',
                            headers='firstrow', floatfmt=('', '.3f', "")))
 
+
 class Nuton:
+    # метод Ньютона
     def __init__(self, startx:  float,
                  endx: float, eps: float):
         self.nullx = startx
         self.nestx = endx
         self.eps = eps
         self.n = 0
+        self.table = [['N', 'x', 'f(x)', 'Fp(x)', '|x(n)-x(n-1)|'],
+                      [self.n, self.nullx, func(self.nullx),
+                       - 1 * (self.nullx + 1) / self.nullx]]
     def algoritm(self):
         if fabs(self.nestx-self.nullx) < self.eps:
             x = self.nullx
-            return print(self.nullx)
+            return print(tabulate(self.table, tablefmt='pipe', stralign='center',
+                           headers='firstrow', floatfmt=('', '', "")))
         else:
-            print(self.nullx,self.nestx)
+            self.n += 1
             self.nestx, self.nullx = self.nullx - (2 - self.nullx - log(self.nullx)) /\
                                        (- 1 * ((self.nullx + 1) / self.nullx)), self.nestx
-            print(self.nullx, self.nestx)
+            self.table.append([self.n, self.nullx, func(self.nullx),
+                               - 1 * (self.nullx + 1) / self.nullx])
             Nuton.algoritm(self)
 
+
 class Xord:
+    # метод Хорд
     def __init__(self, start, end, eps):
         self.start = start
         self.end = end
         self.startstart = self.start - func(self.start) *\
                           (self.end - self.start) /\
                           (func(self.end) - func(self.start))
-
         self.eps = eps
         self.n = 0
+        self.table = [['N', 'X(n + 1)', 'x(n-1)', 'x(n)', 'f(x(n-1))', 'f(x(n))', '|x(n)-x(n-1)|'],
+                      [self.n, self.startstart, self.start,
+                       self.end, func(self.start),
+                       func(self.end), fabs(self.end - self.start)]]
     def algoritm(self):
         if fabs(self.startstart - self.end) < self.eps:
-            return print(self.start,self.startstart,self.end)
+            self.n += 1
+            self.start = self.end
+            self.end = self.startstart
+            self.startstart = self.start - func(self.start) * \
+                              (self.end - self.start) / \
+                              (func(self.end) - func(self.start))
+
+            self.table.append([self.n, self.startstart, self.start,
+                               self.end, func(self.start),
+                               func(self.end), fabs(self.end - self.start)])
+            return print(tabulate(self.table, tablefmt='pipe', stralign='center',
+                           headers='firstrow', floatfmt=('', '', '','')))
         else:
+            self.n += 1
             self.start = self.end
             self.end = self.startstart
             self.startstart = self.start - func(self.start) *\
                           (self.end - self.start) /\
                           (func(self.end) - func(self.start))
 
-            print(self.start, self.startstart, self.end)
+            self.table.append([self.n, self.startstart, self.start,
+                               self.end, func(self.start),
+                               func(self.end), fabs(self.end - self.start)])
             Xord.algoritm(self)
+
+
 class Dima:
-    def __init__(self,ladno,eps):
+    # метод простых итераци
+    def __init__(self, ladno, eps):
         self.start = ladno
         self.next = Denis(self.start)
         self.eps = eps
         self.n = 0
+        self.table = [['N', 'x(n-1)', 'x(n)', '|x(n-1)-x(n)|'],
+                      [self.n, self.start, self.next, fabs(self.next - self.start)]]
+
     def algoritm(self):
+
         if fabs(self.next - self.start) < self.eps:
-            return print(self.start,self.next)
-        else:
+            self.n += 1
             self.start = self.next
             self.next = Denis(self.start)
+            self.table.append([self.n, self.start, self.next, fabs(self.next - self.start)])
+            return print(tabulate(self.table, tablefmt='pipe', stralign='center',
+                           headers='firstrow', floatfmt=('', '.3f', "")))
+        else:
+            self.n += 1
+            self.start = self.next
+            self.next = Denis(self.start)
+            self.table.append([self.n, self.start, self.next, fabs(self.next - self.start)])
             Dima.algoritm(self)
 
 
 def func(qq):
     a = 2 - qq - log(qq)
     return a
+
+
 def Denis(qq):
     a = 2 - log(qq)
     return a
+
+
 bisekta(1.4, 1.6).algoritm()
-#(Nuton(1.4, 1.6, 0.00001).algoritm())
-#(Xord(1.4, 1.6, 0.00001).algoritm())
-Dima(1.5,0.0001).algoritm()
+(Nuton(1.5, 1.6, 0.00001).algoritm())
+(Xord(1.4, 1.6, 0.00001).algoritm())
+Dima(1.6,0.000001).algoritm()
